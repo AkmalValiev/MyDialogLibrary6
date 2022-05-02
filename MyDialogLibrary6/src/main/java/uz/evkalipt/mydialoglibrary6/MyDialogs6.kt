e1ba2f15b103270acd8eabcc6947674faa80950e
AkmalValiev <akki77777@mail.ru>
2022-05-02 10:15:23 +0500

    add lib

--- MyDialogLibrary6/src/main/java/uz/evkalipt/mydialoglibrary6/MyDialogs6.kt
+++ MyDialogLibrary6/src/main/java/uz/evkalipt/mydialoglibrary6/MyDialogs6.kt
@@ -1,8 +1,14 @@
 package uz.evkalipt.mydialoglibrary6
 
 import android.app.AlertDialog
+import android.app.DatePickerDialog
 import android.content.Context
+import android.os.Build
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.color.colorChooser
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 object MyDialogs6 {
 
@@ -31,6 +37,31 @@ object MyDialogs6 {
             Toast.makeText(context, itemList[w], Toast.LENGTH_SHORT).show()
         }
         alertDialog.show()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun calendarDialog(context: Context){
+        val pickerDialog = DatePickerDialog(context)
+        pickerDialog.show()
+    }
+
+    fun chekWifi(context: Context){
+        val bottomSheetDialog = BottomSheetDialog(context)
+        bottomSheetDialog.setContentView(R.layout.item)
+        bottomSheetDialog.show()
+    }
+
+    fun colors(context: Context, title: String, colors:IntArray){
+        MaterialDialog(context).show {
+            title(0, title)
+            colorChooser(colors) { _, color ->
+                Toast.makeText(context, color.toString(), Toast.LENGTH_SHORT).show()
+            }
+            negativeButton(null, "Cancel") {
+                it.dismiss()
+            }
+            positiveButton(null, "Select")
+        }
     }
 
 }
